41c79deacacd3a2d5be05e238e6806ca014a3e46
Joanna Chądzyńska <jmalek87@gmail.com>
2020-05-04 21:30:59 +0200

    added data.js for storing buttons informations

---
+++ src/components/Main/data.js
@@ -0,0 +1,160 @@
+export const calcButtons = [
+	{
+		id: "js-percent",
+		className: "calculator__button",
+		content: "%",
+	},
+	{
+		id: "js-cancel",
+		className: "calculator__button",
+		content: "CE",
+	},
+	{
+		id: "js-clear",
+		className: "calculator__button",
+		content: "C",
+	},
+	{
+		id: "js-back",
+		className: "calculator__button",
+		content: "<",
+	},
+	{
+		id: "js-fraction",
+		className: "calculator__button",
+		content: "1/x",
+	},
+	{
+		id: "js-power",
+		className: "calculator__button",
+		content: "x(2)",
+	},
+	{
+		id: "js-square",
+		className: "calculator__button",
+		content: "sqrt",
+	},
+	{
+		id: "js-divide",
+		className: "calculator__button",
+		content: "/",
+	},
+	{
+		id: "num-7",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "7",
+	},
+	{
+		id: "num-8",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "8",
+	},
+	{
+		id: "num-9",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "9",
+	},
+	{
+		id: "js-multiply",
+		className: "calculator__button",
+		content: "x",
+	},
+	{
+		id: "num-4",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "4",
+	},
+	{
+		id: "num-5",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "5",
+	},
+	{
+		id: "num-6",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "6",
+	},
+	{
+		id: "js-subtraction",
+		className: "calculator__button",
+		content: "-",
+	},
+	{
+		id: "num-1",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "1",
+	},
+	{
+		id: "num-2",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "2",
+	},
+	{
+		id: "num-3",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "3",
+	},
+	{
+		id: "js-addition",
+		className: "calculator__button",
+		content: "+",
+	},
+	{
+		id: "js-invert",
+		className: "calculator__button calculator__button--is-light",
+		content: "+/-",
+	},
+	{
+		id: "num-0",
+		className:
+			"calculator__button calculator__button--is-number calculator__button--is-light",
+		content: "0",
+	},
+	{
+		id: "js-comma",
+		className: "calculator__button calculator__button--is-light",
+		content: ",",
+	},
+	{
+		id: "js-equal",
+		className: "calculator__button",
+		content: "=",
+	},
+];
+
+export const memoryButtons = [
+	{
+		id: "js-MC",
+		className: "calculator__memory-button",
+		content: "MC",
+	},
+	{
+		id: "js-MR",
+		className: "calculator__memory-button",
+		content: "MR",
+	},
+	{
+		id: "js-M+",
+		className: "calculator__memory-button",
+		content: "M+",
+	},
+	{
+		id: "js-M-",
+		className: "calculator__memory-button",
+		content: "M-",
+	},
+	{
+		id: "js-MS",
+		className: "calculator__memory-button",
+		content: "MS",
+	},
+];
