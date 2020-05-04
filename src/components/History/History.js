1782a6ef9ffe5a3d946e1ae5aaddaf8f081e0193
Joanna Chądzyńska <jmalek87@gmail.com>
2020-05-04 21:30:19 +0200

    added history display

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -2,11 +2,18 @@ import React from "react";
 
 const History = ({ store }) => {
 	const [state] = store;
+	const { historyDisplay } = state;
 
 	return (
 		<div className='calculator__history-wrapper'>
 			<p className='calculator__history-title'>History:</p>
-			<div className='calculator__container'>{state.historyDisplay}</div>
+			<div className='calculator__container'>
+				<p className='calculator__history-element'>
+					{!historyDisplay
+						? null
+						: `${historyDisplay.firstValue} ${historyDisplay.char} ${historyDisplay.secondValue} = ${historyDisplay.result}`}
+				</p>
+			</div>
 		</div>
 	);
 };
